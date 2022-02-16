9bd8f489789b0801b94855873dec1013ebe47e01
Baptiste <bdauphouy@gmail.com>
2022-02-16 12:32:50 +0100

    feat: end game screens and restart

--- main.js
+++ main.js
@@ -2,15 +2,3 @@ import Mastermind from './lib/Mastermind'
 
 const mm = new Mastermind()
 mm.init()
-
-const colorListElement = document.querySelector('.color-list')
-
-document.addEventListener('click', e => {
-  if (
-    e.target.matches('.color-list .dots') ||
-    (!e.target.closest('.color-list') &&
-      colorListElement.classList.contains('active'))
-  ) {
-    colorListElement.classList.toggle('active')
-  }
-})
