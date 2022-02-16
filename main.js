7f6195b8d4908b19047ce511abf725dd68beb19b
Baptiste <bdauphouy@gmail.com>
2022-02-16 20:18:21 +0100

    feat: dark mode

--- main.js
+++ main.js
@@ -2,3 +2,23 @@ import Mastermind from './lib/Mastermind'
 
 const mm = new Mastermind()
 mm.init()
+
+const handleTheme = () => {
+  const switchThemeButton = document.querySelector('.switch-theme-button')
+
+  let theme = window.localStorage.getItem('mastermind-theme')
+
+  if (theme) {
+    document.body.className = theme
+  } else {
+    window.localStorage.setItem('mastermind-theme', 'light')
+  }
+
+  switchThemeButton.addEventListener('click', () => {
+    theme = theme === 'dark' ? 'light' : 'dark'
+    window.localStorage.setItem('mastermind-theme', theme)
+    document.body.className = theme
+  })
+}
+
+handleTheme()
