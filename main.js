320ca32286073c2ba4457ef71da6d260094c482c
Baptiste <bdauphouy@gmail.com>
2022-02-20 14:33:15 +0100

    feat: mobile accessibility improved

--- main.js
+++ main.js
@@ -2,24 +2,3 @@ import Mastermind from './lib/Mastermind'
 
 const mm = new Mastermind()
 mm.init()
-
-const handleTheme = () => {
-  const switchThemeButton = document.querySelector('.switch-theme-button')
-
-  let theme = window.localStorage.getItem('mastermind-theme')
-
-  if (theme) {
-    document.body.className = theme
-  } else {
-    window.localStorage.setItem('mastermind-theme', 'light')
-    document.body.className = 'light'
-  }
-
-  switchThemeButton.addEventListener('click', () => {
-    theme = theme === 'dark' ? 'light' : 'dark'
-    window.localStorage.setItem('mastermind-theme', theme)
-    document.body.className = theme
-  })
-}
-
-handleTheme()
